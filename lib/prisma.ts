a5214ee4d7865ac7788e388df23387e95538f47f
Revel <revelcahyadi2004@gmail.com>
2026-06-10 10:01:39 +0700

    fix : fixing of prisma client singleton

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,20 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 // Mencegah koneksi berlipat ganda saat hot-reload di Next.js
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// Fungsi untuk membuat instance Prisma baru beserta adapternya
+const prismaClientSingleton = () => {
+  // Hubungkan ke URL Neon milikmu
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+  // Suntikkan adapter ke dalam PrismaClient
+  return new PrismaClient({ adapter });
+};
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
